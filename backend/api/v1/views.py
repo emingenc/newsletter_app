20b3f7f9442e39f2fd21d4847682e81fbe253921
emingenc <emingenc@novit.ai>
2022-03-10 19:33:05 +0300

    ADD: newsletter model

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, request
+from flask import Blueprint
 from flask.json import jsonify
 
 
 v1 = Blueprint("v1", __name__, url_prefix="/api/v1")
 
-v1.route("/")(lambda: jsonify({"message": "Hello, World!"}))
+v1.route("/ping")(lambda: jsonify({"message": "pong"}))
