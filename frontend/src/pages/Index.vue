52f5d5980b5ac219043e92bdf7752e82bbc17340
emingenc <emingenc@novit.ai>
2022-03-13 21:43:03 +0300

    add: static frontend for quickstart docker compose

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="flex flex-center q-pa-xl q-gutter-xl">
+    <div class="flex fit flex-center q-pa-xl q-gutter-xl">
 
     <Newsletter  v-for="newsletter in newsletters"
      :key="newsletter.id" :id="newsletter.id" 
